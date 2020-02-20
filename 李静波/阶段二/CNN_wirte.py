110de7bfab4fb45e4f712f177e1cbb107d4846f0
7JellyLee <906139088@qq.com>
2020-02-20 15:46:16 +0800

    阶段二
    阶段二

---
+++ 李静波/阶段二/CNN_wirte.py
@@ -0,0 +1,131 @@
+from torch.utils.data import DataLoader,Dataset
+from skimage import io,transform
+import matplotlib.pyplot as plt
+import os
+import torch
+from torchvision import transforms
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.utils.data as Data
+import torchvision
+
+
+EPOCH = 1
+BATCH_SIZE = 50
+LR = 0.001
+
+class Data(Dataset):
+    def __init__(self, root_dir, transform=None):
+        self.root_dir = root_dir
+        self.transform = transform
+        self.images = os.listdir(self.root_dir)
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, index):
+        image_index = self.images[index]
+        img_path = os.path.join(self.root_dir, image_index)
+        img = io.imread(img_path)
+        label = img_path.split('\\')[-1].split('.')[0]
+        sample = {'image': img, 'label': label}
+
+        if self.transform:
+            sample = self.transform(sample)
+        return sample
+
+if __name__=='__main__':
+    train_data = Data('./write/train',transform=None)
+    train_loader = DataLoader(train_data,batch_size=BATCH_SIZE,shuffle=True)
+    for i_batch,batch_data in enumerate(train_loader):
+        print(i_batch)
+        print(batch_data['image'] / 255)
+        # plt.imshow(train_data.train_data[0].numpy(), cmap='gray')
+        # plt.title('%i' % train_data.train_labels[0])
+        # plt.show()
+    test_data = Data('./write/test', transform=None)
+    test_loader = DataLoader(test_data, batch_size=BATCH_SIZE, shuffle=True)
+    for i_batch,batch_data in enumerate(train_loader):
+        print(i_batch)
+        print(batch_data['image'].size())#batch_data['image']对应train_data.data
+    test_x = torch.unsqueeze(test_data.data, dim=1).type(torch.FloatTensor)[:2000] / 255.  #print(batch_data['image'] / 255)
+    test_y = test_data.test_labels[:2000]
+
+
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=1,
+                out_channels=16,
+                kernel_size=5,
+                stride=1,
+                padding=2,
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(16, 32, 5, 1, 2),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+        )
+        self.out = nn.Linear(32 * 7 * 7, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = x.view(x.size(0), -1)           
+        output = self.out(x)
+        return output, x
+
+
+cnn = CNN()
+print(cnn)
+
+optimizer = torch.optim.Adam(cnn.parameters(), lr=LR)
+loss_func = nn.CrossEntropyLoss()
+
+
+from matplotlib import cm
+try: from sklearn.manifold import TSNE; HAS_SK = True
+except: HAS_SK = False; print('Please install sklearn for layer visualization')
+def plot_with_labels(lowDWeights, labels):
+    plt.cla()
+    X, Y = lowDWeights[:, 0], lowDWeights[:, 1]
+    for x, y, s in zip(X, Y, labels):
+        c = cm.rainbow(int(255 * s / 9)); plt.text(x, y, s, backgroundcolor=c, fontsize=9)
+    plt.xlim(X.min(), X.max()); plt.ylim(Y.min(), Y.max()); plt.title('Visualize last layer'); plt.show(); plt.pause(0.01)
+
+plt.ion()
+# training and testing
+for epoch in range(EPOCH):
+    for step, (b_x, b_y) in enumerate(train_loader):   #
+
+        output = cnn(b_x)[0]
+        loss = loss_func(output, b_y)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if step % 50 == 0:
+            test_output, last_layer = cnn(test_x)
+            pred_y = torch.max(test_output, 1)[1].data.numpy()
+            accuracy = float((pred_y == test_y.data.numpy()).astype(int).sum()) / float(test_y.size(0))
+            print('Epoch: ', epoch, '| train loss: %.4f' % loss.data.numpy(), '| test accuracy: %.2f' % accuracy)
+            if HAS_SK:
+                # Visualization of trained flatten layer (T-SNE)
+                tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=5000)
+                plot_only = 500
+                low_dim_embs = tsne.fit_transform(last_layer.data.numpy()[:plot_only, :])
+                labels = test_y.numpy()[:plot_only]
+                plot_with_labels(low_dim_embs, labels)
+plt.ioff()
+
+test_output, _ = cnn(test_x[:10])
+pred_y = torch.max(test_output, 1)[1].data.numpy()
+print(pred_y, 'prediction number')
+print(test_y[:10].numpy(), 'real number')
